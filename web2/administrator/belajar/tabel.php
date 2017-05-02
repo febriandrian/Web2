f47abf67f37663983a50d5d68b45fe36ec8bab1c
febriandrian <febri.andrian557@gmail.com>
2017-05-02 13:01:02 +0700

    Add files via upload

---
+++ web2/administrator/belajar/tabel.php
@@ -0,0 +1,14 @@
+<table border="1" >
+	<tr>
+		<td>No</td>
+		<td>NIM</td>
+		<td>Nama</td>
+	</tr>
+	<tbody>
+		<tr>
+			<td>1</td>
+			<td>H1101151011</td>
+			<td>Febri Andrian</td>
+		</tr>
+	</tbody>
+</table>
